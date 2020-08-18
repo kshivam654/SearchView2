914663d578552344aa80225e80ec9be3d893930a
Shivam Sharma <kshivam654@gmail.com>
2020-08-18 17:04:35 +0530

    Initial commit + Layout for Search + Firebase Dependencies added -> Travel App

---
+++ app/src/main/java/app/sharma/searchview2/MainActivity.java
@@ -0,0 +1,25 @@
+package app.sharma.searchview2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mSearchField;
+    private ImageButton mSearchBtn;
+
+    private RecyclerView mResultList;
+
+    private DatabaseReference mUserDatabase;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
